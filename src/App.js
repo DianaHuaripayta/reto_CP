378ca5500b62866483f8380c3c25331cf9283a09
DianaHuaripayta <angelicagonzales1610@gmail.com>
2022-04-27 15:42:01 -0500

    add home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { UserAuthContextProvider } from "./context/authContext";
 import { Routes, Route } from "react-router-dom";
-import LogIn from './components/login/Login';
 import SignUp from "./components/login/Signup";
 import Home from "./components/Home/Home";
+import Navbar from "./layout/header/Navbar";
 function App() {
   return (
     <>
     <UserAuthContextProvider>
+      <Navbar></Navbar>
       <Routes>
-            <Route path="/" element={ <LogIn/>} />
+            <Route path="/" element={ <Home/>} />
             <Route path="/signUp" element={ <SignUp/>} />
             <Route path="/home" element={ <Home/>} />
       </Routes>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,63 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import {ImageButton, ImageSrc, Image, ImageBackdrop, ImageMarked} from './homeStyled'
+import { Link } from "react-router-dom";
 export default function Home() {
+  const [premieres, setPremieres] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://ec2-3-138-85-219.us-east-2.compute.amazonaws.com:8080/cp/v1/premieres')
+    .then((response) => {
+      setPremieres(response.data.premieres)
+      console.log(response.data.premieres)
+
+    });
+  }, []);
   return (
-    <div>Home</div>
+    <>
+   <Typography
+            variant="h5"
+            noWrap
+            component="div"
+            sx={{ margin: '30px 50px', fontWeight:'bolder', display: { xs: 'none', md: 'flex' } }}
+          >
+            Premieres
+    </Typography>
+   <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '95%' , margin: 'auto'}}>
+    {premieres.map((item, index) => 
+      <ImageButton
+        focusRipple
+        component={Link}
+        to="/signUp"
+        key={index}
+        style={{  
+          width: '30%',
+          margin: 20,
+        }}
+      >
+        <ImageSrc style={{ backgroundImage: `url(${item.image})` }} />
+        <ImageBackdrop className="MuiImageBackdrop-root" />
+        <Image>
+          <Typography
+            component="span"
+            variant="subtitle1"
+            color="inherit"
+            sx={{
+              position: 'relative',
+              p: 4,
+              pt: 2,
+              pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+            }}
+          >
+            {item.description}
+            <ImageMarked className="MuiImageMarked-root" />
+          </Typography>
+        </Image>
+      </ImageButton>
+    )}
+  </Box>
+  </>
   )
 }
